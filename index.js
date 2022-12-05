dd2f33f1c24c575d2b735637b01246b0179b6039
gmpappady <georgempappady@gmail.com>
2022-12-05 22:54:52 +0530

    qwerty

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
 import express, { application } from "express"
 import itemRoutes from "./routes/items.js"
+import authRoutes from "./routes/auth.js"
+import userRoutes from "./routes/users.js"
 
 const app=express()
+
+
 app.use(express.json())
 app.use("/api/items",itemRoutes)
+app.use("/api/auth",authRoutes)
+app.use("/api/users",userRoutes)
+
 
 app.get("/test",(req,res)=>{
     res.json("it works!")
