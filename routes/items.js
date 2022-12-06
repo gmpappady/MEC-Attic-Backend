49fc7247129645fd88ca7958da31e0781400e321
emilin-20 <emilinseby3@gmail.com>
2022-12-06 08:39:36 +0530

    fetching data

--- routes/items.js
+++ routes/items.js
@@ -1,7 +1,12 @@
 import express from "express"
-import { addItem } from "../controllers/item.js"
+import { addItem, deleteItem, getItem, getItems, updateItem } from "../controllers/item.js"
 const router=express.Router()
 
+router.get("/",getItems)
+router.get("/:id",getItem)
+router.post("/",addItem)
+router.delete("/:id ",deleteItem)
+router.update("/:id",updateItem)
 
 
 export default router
