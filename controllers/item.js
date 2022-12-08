ef3741309dbaa5a92a4e91190227cfb0ae4aa4f3
gmpappady <georgempappady@gmail.com>
2022-12-08 23:45:19 +0530

    product page and seller page almost done

--- controllers/item.js
+++ controllers/item.js
@@ -2,50 +2,66 @@ import {db} from "../db.js"
 
 
 export const getItems  =(req,res)=>{
-    const q = "SELECT * FROM items ORDER BY RAND() LIMIT 9";
+    const q = "SELECT * FROM items ORDER BY RAND() LIMIT 9" ;
 
     db.query(q,[req.body],(err,data)=> {
         if (err) return res.json(err)
-        console.log(data)
+        //console.log(data)
         return res.status(200).json(data)
     });
 };
 
 export const getItem=(req,res)=>{
-    const q =
-    "SELECT id, `name`, `category`, `desc`, img, , seller_id ,  `name`, bargainable`,  FROM users u JOIN posts p ON u.id = p.uid WHERE p.id = ? ";
+  console.log(req.params.id)  
+  const q =
+    "SELECT id, `name`, `category`, `desc`, img, , seller_id ,  `name`, bargainable`  FROM users u JOIN posts p ON u.id = p.id WHERE p.id = ? ";
 
   db.query(q, [req.params.id], (err, data) => {
-    if (err) return res.status(500).json(err);
-
+    if (err) 
+    {
+      console.log(err)
+    return res.status(500).json(err);
+    }
+    console.log(data)
     return res.status(200).json(data[0]);
   });
 };
+
+
+
+
 export const addItem=(req,res)=>{
-    const token = req.cookies.access_token;
-  if (!token) return res.status(401).json("Not authenticated!");
+  //   const token = req.cookies.access_token;
+  // if (!token) return res.status(401).json("Not authenticated!");
 
-  jwt.verify(token, "jwtkey", (err, userInfo) => {
-    if (err) return res.status(403).json("Token is not valid!");
+  // jwt.verify(token, "jwtkey", (err, userInfo) => {
+  //   if (err) return res.status(403).json("Token is not valid!");
 
+  console.log("wertyui",req.body)
+  const {name,category,price,desc,seller_id}=req.body
     const q =
-      "INSERT INTO posts(`title`, `desc`, `img`, `cat`, `date`,`uid`) VALUES (?)";
+      "INSERT INTO mec-attic.items(name, category, price, desc,seller_id) VALUES (?)";
 
     const values = [
-      req.body.title,
+      req.body.name,
+      req.body.category,
+      req.body.price,
       req.body.desc,
-      req.body.img,
-      req.body.cat,
-      req.body.date,
-      userInfo.id,
+      req.body.seller_id
     ];
 
     db.query(q, [values], (err, data) => {
-      if (err) return res.status(500).json(err);
+      if (err) {
+        console.log(err) 
+        return res.status(500).json(err);
+      }
       return res.json("Post has been created.");
     });
-  });
+  
 };
+
+
+
 export const  deleteItem=(req,res)=>{
     res.json("from controller")
 };
